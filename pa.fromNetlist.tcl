182bfe2938d4014cc1e874050d8ce2b81657b931
engrgba <malikgbancc@gmail.com>
2024-06-13 17:03:46 +0500

    top module added and code arranged

--- pa.fromNetlist.tcl
+++ pa.fromNetlist.tcl
@@ -1,10 +1,10 @@
 
 # PlanAhead Launch Script for Post-Synthesis pin planning, created by Project Navigator
 
-create_project -name kit7seg -dir "D:/Gul Bahar Data/FPGAs/XILINX ISE/New folder/kit7seg/planAhead_run_1" -part xc6slx9tqg144-2
+create_project -name kit7seg -dir "D:/Gul Bahar Data/FPGAs/XILINX ISE/New folder/updated kit7seg/planAhead_run_3" -part xc6slx9tqg144-2
 set_property design_mode GateLvl [get_property srcset [current_run -impl]]
-set_property edif_top_file "D:/Gul Bahar Data/FPGAs/XILINX ISE/New folder/kit7seg/Wishbone_Controller.ngc" [ get_property srcset [ current_run ] ]
-add_files -norecurse { {D:/Gul Bahar Data/FPGAs/XILINX ISE/New folder/kit7seg} {ipcore_dir} }
+set_property edif_top_file "D:/Gul Bahar Data/FPGAs/XILINX ISE/New folder/updated kit7seg/KIT7_SEG_TOP.ngc" [ get_property srcset [ current_run ] ]
+add_files -norecurse { {D:/Gul Bahar Data/FPGAs/XILINX ISE/New folder/updated kit7seg} {ipcore_dir} }
 add_files [list {ipcore_dir/icon.ncf}] -fileset [get_property constrset [current_run]]
 add_files [list {ipcore_dir/ila.ncf}] -fileset [get_property constrset [current_run]]
 set_param project.pinAheadLayout  yes
